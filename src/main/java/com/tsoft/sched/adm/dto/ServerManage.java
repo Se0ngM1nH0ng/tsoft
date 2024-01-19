8a6b2c91f342d34448d6f5cb416f7928d79e28c4
개발 <tjdals9219@naver.com>
2024-01-19 14:53:50 +0900

    주석 수정

--- src/main/java/com/tsoft/sched/adm/dto/ServerManage.java
+++ src/main/java/com/tsoft/sched/adm/dto/ServerManage.java
@@ -5,12 +5,12 @@ import lombok.Data;
 @Data
 public class ServerManage {
 
-    private int serverId;
-    private String serverGroup;
-    private String serverName;
-    private String serverIp;
-    private String serverDesc;
+    private int serverId; // 번호
+    private String serverGroup; // 서버그룹 
+    private String serverName; // 서버명
+    private String serverIp; // 서버ip
+    private String serverDesc; // 서버 내용
 
-    private String useYn;
+    private String useYn; // 노출여부
 
 }
